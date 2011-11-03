8919f29407d400bad65f4bb89a5285aced999862
J.T.Sage <jtsage+tdtrac@gmail.com>
2011-11-03 17:21:29 -0400

    Update demos to RC2 / jQ 1.6.4

--- demos/extras.js
+++ demos/extras.js
@@ -1,24 +1,2 @@
-//set up the theme switcher on the homepage
-$('div').live('pagecreate', function(event) {
-    if (!$(this).is('.ui-dialog')) {
-        var appendEl = $(this).find('.ui-footer:last');
+//do nothing for now.
 
-        if (!appendEl.length) {
-            appendEl = $(this).find('.ui-content');
-        }
-
-        if (appendEl.is("[data-position]")) {
-            return;
-        }
-
-        $('<a href="#themeswitcher" data-' + $.mobile.ns + 'rel="dialog" data-' + $.mobile.ns + 'transition="pop">Switch theme</a>').buttonMarkup({
-            'icon': 'gear',
-            'inline': true,
-            'shadow': false,
-            'theme': 'd'
-        }).appendTo(appendEl.find('.ui-controlgroup')).wrap('<div class="jqm-themeswitcher">').bind("vclick", function() {
-            $.themeswitcher();
-        });
-    }
-
-});
