fa0677c146f47d1ed48e0c8c1f35d0f4eef03a3f
Robin Heggelund Hansen <skinneyz89@gmail.com>
2011-11-15 12:59:24 +0100

    Made some tweaks, added credits and changed norwegian letters into html representation for better portability.

--- js/i8n/jquery.mobile.datebox.i8n.nb-NO.js
+++ js/i8n/jquery.mobile.datebox.i8n.nb-NO.js
@@ -1,13 +1,23 @@
+/*
+ * jQuery Mobile Framework : plugin to provide a date and time picker.
+ * Copyright (c) JTSage
+ * CC 3.0 Attribution.  May be relicensed without permission/notifcation.
+ * https://github.com/jtsage/jquery-mobile-datebox
+ *
+ * Norwegian localisation for JQM DateBox plugin *
+ * Written by: Robin Heggelund Hansen (skinneyz89@gmail.com)
+ */
+
 jQuery.extend(jQuery.mobile.datebox.prototype.options.lang, {
     'nb-NO': {
         setDateButtonLabel: 'Velg dato',
         setTimeButtonLabel: 'Velg tidspunkt',
         setDurationButtonLabel: 'Velg varighet',
-        calTodayButtonLabel: 'Gå til dagens dato',
+        calTodayButtonLabel: 'G&#229; til dagens dato',
         titleDateDialogLabel: 'Velg dato',
         titleTimeDialogLabel: 'Velg tidspunkt',
-        daysOfWeek: ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
-        daysOfWeekShort: ['Søn','Man','Tirs','Ons','Tors','Fre','Lør'],
+        daysOfWeek: ['S&#248;ndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','L&#248;rdag'],
+        daysOfWeekShort: ['S&#248;n','Man','Tirs','Ons','Tors','Fre','L&#248;r'],
         monthsOfYear: ['januar','februar','mars','april','mai','juni','juli','august','september','oktober','november','desember'],
         monthsOfYearShort: ['jan','feb','mar','apr','mai','jun','jul','aug','sep','okt','nov','des'],
         durationLabel: ['Dager','Timer','Minutter','Sekunder'],
