48e5fded63a35cc8fdac96850a70da0c0db4ffc8
J.T.Sage <jtsage+tdtrac@gmail.com>
2012-02-06 20:29:54 -0500

    Update "uk" to gettext

--- i18n/translators.py
+++ i18n/translators.py
@@ -4,4 +4,5 @@ trans = {
 	'en': 'J.T.Sage <jtsage@gmail.com>',
 	'en-US': 'J.T.Sage <jtsage@gmail.com>',
 	'zh-CH': 'ChiElvis <elvis311@msn.com>',
+	'uk': 'Alexander Zolotko <azolotko@gmail.com>',
 }
