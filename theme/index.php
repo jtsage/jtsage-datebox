49fbe7c1d02293b4b7740b77681c375908e191a8
J.T.Sage <jtsage+tdtrac@gmail.com>
2012-03-12 22:00:56 -0400

    Add theming control to repo

---
+++ theme/index.php
@@ -0,0 +1,220 @@
+<?php
+
+$defaults = array(
+	'cgdw' => '36', // Cal box width (calbox)
+	'cgdh' => '30', // Cal box height (calbox)
+	'cgdl' => '30', // Cal box line-height (calbox)
+	'cgfs' => '12', // Cal box font size (calbox)
+	'cgdec' => '#888', // Empty date color (calbox)
+	'cgddc' => '#888', // Disabled date color (calbox)
+	'dcw' => '77', // button width (datebox)
+	'dsiw' => '74', // input width (datebox)
+	'width' => '280', // Master Width
+	'fw' => '77', // width (flipbox)
+	'fwl' => '260', // lens width (flipbox)
+	'fh' => '30', // cell heigh (flipbox)
+	'fhl' => '40', // lens height (flipbox)
+	'fch' => '125', // content height (flipbox)
+	'fcsh' => '120', // scroller height (flipbox)
+	'ssyw' => 84,
+	'ssyl' => 30,
+	'ssyh' => 30,
+	'ssyf' => 14,
+	'ssmw' => 51,
+	'ssml' => 30,
+	'ssmh' => 30,
+	'ssmf' => 12,
+	'ssdw' => 32,
+	'ssdl' => 20,
+	'ssdh' => 38,
+	'ssdf' => 14,
+	'sshw' => 32,
+	'sshl' => 22,
+	'sshh' => 24,
+	'sshf' => 14,
+	'swdf' => 10
+);
+
+$use = array();
+
+foreach ( $defaults as $key => $value ) {
+	if ( isset($_REQUEST[$key]) ) {
+		$use[$key] = $_REQUEST[$key];
+	} else {
+		$use[$key] = $value;
+	}
+}
+?>
+<!DOCTYPE html> 
+<html lang="en" class="ui-body-b"> 
+<head> 
+	<!--meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /--> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>jQueryMobile - DateBox Demo</title>
+	<link href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.css" rel="stylesheet" type="text/css" />
+	
+	<?php if ( !empty($_SERVER['QUERY_STRING']) ) {
+		echo '<link type="text/css" href="sheet.php?'.$_SERVER['QUERY_STRING'].'" rel="stylesheet" />'."\n";
+	} else {
+		echo '<link type="text/css" href="sheet.php" rel="stylesheet" />'."\n";
+	} ?>
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script> 
+	<!--script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script-->
+	<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jquery.mousewheel.min.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jQM-DateBox2/js/jquery.mobile.datebox.core.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jQM-DateBox2/js/jquery.mobile.datebox.calbox.min.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jQM-DateBox2/js/jquery.mobile.datebox.datebox.min.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jQM-DateBox2/js/jquery.mobile.datebox.flipbox.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/jQM-DateBox2/js/jquery.mobile.datebox.slidebox.js"></script>
+	<script type="text/javascript" src="http://dev.jtsage.com/cdn/datebox/i18n/jquery.mobile.datebox.i18n.en_US.utf8.js"></script>
+	
+	<script type="text/javascript">
+		$(document).ready( function() {
+			$('#display').find('input').val('0');
+			$('.applysheet').click(function () {
+				theList = $('#css').serialize();
+				$("link[href*=sheet\\.php]:last").after('<link href="sheet.php?' + theList + '" type="text/css" rel="Stylesheet" />');
+				if ($("link[href*=sheet\\.php]").size() > 2) {
+					$("link[href*=sheet\\.php]:first").remove();
+				}
+			});
+			
+			$('#css input[type=checkbox]').change(function() {
+				$('#' + $(this).attr('id') + 'div').toggle();
+			});
+			$('#css input').change(function() {
+				var theLink = "index.php?" + $('#css').serialize();
+				$('#bookmark').attr('href', theLink);
+			});
+			$('#getsheet').click(function () {
+				theList = $('#css').serialize();
+				location.href = "sheet.php?" + theList;
+			});
+		});
+	</script>
+	<script type="text/javascript">
+		jQuery.extend(jQuery.mobile.datebox.prototype.options, {
+			useInline: true,
+			hideInput: true,
+		});
+	</script>
+	<style>
+		label.ui-input-text { font-size: .8em !important; vertical-align: middle !important;}
+		.ui-datebox-grid { clear: right !important; }
+	</style>
+	
+</head>
+<body style="margin: 0">
+	<div class="ui-header ui-bar-a" role="banner" style="position: relative;"> 
+		<h1 class="ui-title" tabindex="0" role="heading" aria-level="1">jQM-DateBox - Theme Roller</h1>
+	</div>
+	
+	<div data-role="ui" style="width: 550px; float: left;">
+		<form id="css" style="padding-left: 30px;">
+			<div data-role="fieldcontain">
+				<label for="width">Master Width</label>
+				<input name="width" id="width" value="<?php echo $use['width']; ?>" type="range" min="10" max="700">
+				<label for="cgddc">Master Disable Date Color</label>
+				<input name="cgddc" id="cgddc" value="<?php echo $use['cgddc']; ?>" type="text">
+			</div>
+			<a class="applysheet" href="#" data-role="button" data-theme="b">Apply Changes</a>
+			
+			<h2>CalBox</h2>
+			<div data-role="fieldcontain">
+				<label for="cgdw">Date Width</label>
+				<input name="cgdw" id="cgdw" value="<?php echo $use['cgdw']; ?>" type="range" min="5" max="100">
+				<label for="cgdh">Date Height</label>
+				<input name="cgdh" id="cgdh" value="<?php echo $use['cgdh']; ?>" type="range" min="5" max="100">
+				<label for="cgdl">Date Line-Height</label>
+				<input name="cgdl" id="cgdl" value="<?php echo $use['cgdl']; ?>" type="range" min="5" max="100">
+				<label for="cgfs">Date Font Size</label>
+				<input name="cgfs" id="cgfs" value="<?php echo $use['cgfs']; ?>" type="range" min="5" max="40">
+				<label for="cgdec">Different Month Color</label>
+				<input name="cgdec" id="cgdec" value="<?php echo $use['cgdec']; ?>" type="text">
+			</div>
+			<a class="applysheet" href="#" data-role="button" data-theme="b">Apply Changes</a>
+			
+			<h2>DateBox</h2>
+			<div data-role="fieldcontain">
+				<label for="dcw">Button Width</label>
+				<input name="dcw" id="dcw" value="<?php echo $use['dcw']; ?>" type="range" min="5" max="150">
+				<label for="dsiw">Input Width</label>
+				<input name="dsiw" id="dsiw" value="<?php echo $use['dsiw']; ?>" type="range" min="5" max="150">
+			</div>
+			<a class="applysheet" href="#" data-role="button" data-theme="b">Apply Changes</a>
+			
+			<h2>FlipBox</h2>
+			<div data-role="fieldcontain">
+				<label for="fw">Roller Width</label>
+				<input name="fw" id="fw" value="<?php echo $use['fw']; ?>" type="range" min="5" max="150">
+				<label for="fwl">Lens Width</label>
+				<input name="fwl" id="fwl" value="<?php echo $use['fwl']; ?>" type="range" min="5" max="550">
+				<label for="fh">Cell Height</label>
+				<input name="fh" id="fh" value="<?php echo $use['fh']; ?>" type="range" min="5" max="150">
+				<label for="fhl">Lens Height</label>
+				<input name="fhl" id="fhl" value="<?php echo $use['fhl']; ?>" type="range" min="5" max="150">
+				<label for="fch">Content Height</label>
+				<input name="fch" id="fch" value="<?php echo $use['fch']; ?>" type="range" min="5" max="350">
+				<label for="fcsh">Scroller Height (-5px from Content usually)</label>
+				<input name="fcsh" id="fcsh" value="<?php echo $use['fcsh']; ?>" type="range" min="5" max="350">
+			</div>
+			<a class="applysheet" href="#" data-role="button" data-theme="b">Apply Changes</a>
+
+			<h2>SlideBox</h2>
+			<div data-role="fieldcontain">
+				<label for="ssyw">Width - Year</label>
+				<input name="ssyw" id="ssyw" value="<?php echo $use['ssyw']; ?>" type="range" min="5" max="200">
+				<label for="ssmw">Width - Month</label>
+				<input name="ssmw" id="ssmw" value="<?php echo $use['ssmw']; ?>" type="range" min="5" max="200">
+				<label for="ssdw">Width - Day</label>
+				<input name="ssdw" id="ssdw" value="<?php echo $use['ssdw']; ?>" type="range" min="5" max="200">
+				<label for="sshw">Width - Time</label>
+				<input name="sshw" id="sshw" value="<?php echo $use['sshw']; ?>" type="range" min="5" max="200">
+				
+				<label for="ssyl">Line Height - Year</label>
+				<input name="ssyl" id="ssyl" value="<?php echo $use['ssyl']; ?>" type="range" min="5" max="100">
+				<label for="ssml">Line Height - Month</label>
+				<input name="ssml" id="ssml" value="<?php echo $use['ssml']; ?>" type="range" min="5" max="100">
+				<label for="ssdl">Line Height - Day</label>
+				<input name="ssdl" id="ssdl" value="<?php echo $use['ssdl']; ?>" type="range" min="5" max="100">
+				<label for="sshl">Line Height - Time</label>
+				<input name="sshl" id="sshl" value="<?php echo $use['sshl']; ?>" type="range" min="5" max="100">
+				
+				<label for="ssyh">Height - Year</label>
+				<input name="ssyh" id="ssyh" value="<?php echo $use['ssyh']; ?>" type="range" min="5" max="100">
+				<label for="ssmh">Height - Month</label>
+				<input name="ssmh" id="ssmh" value="<?php echo $use['ssmh']; ?>" type="range" min="5" max="100">
+				<label for="ssdh">Height - Day</label>
+				<input name="ssdh" id="ssdh" value="<?php echo $use['ssdh']; ?>" type="range" min="5" max="100">
+				<label for="sshh">Height - Time</label>
+				<input name="sshh" id="sshh" value="<?php echo $use['sshh']; ?>" type="range" min="5" max="100">
+				
+				<label for="ssyf">Font Size - Year</label>
+				<input name="ssyf" id="ssyf" value="<?php echo $use['ssyf']; ?>" type="range" min="5" max="50">
+				<label for="ssmf">Font Size - Month</label>
+				<input name="ssmf" id="ssmf" value="<?php echo $use['ssmf']; ?>" type="range" min="5" max="50">
+				<label for="ssdf">Font Size - Day</label>
+				<input name="ssdf" id="ssdf" value="<?php echo $use['ssdf']; ?>" type="range" min="5" max="50">
+				<label for="sshf">Font Size - Time</label>
+				<input name="sshf" id="sshf" value="<?php echo $use['sshf']; ?>" type="range" min="5" max="50">
+				<label for="swdf">Font Size - Subscripts</label>
+				<input name="swdf" id="swdf" value="<?php echo $use['swdf']; ?>" type="range" min="5" max="50">
+			</div>
+			<a class="applysheet" href="#" data-role="button" data-theme="b">Apply Changes</a>
+
+		</form>
+		<a id="getsheet" href="#" data-role="button" data-theme="b">Get Stylesheet</a>
+		<a id="bookmark" href="#" data-role="button" data-theme="b">Bookmark this Version</a>
+	</div>
+	
+	<div id="display" style="margin-left: 553px; margin-top: 180px">
+		<input name="theme1" id="theme1" type="text" data-role="datebox" data-options='{"mode":"calbox"}' />
+		<input name="theme2" id="theme2" type="text" data-role="datebox" data-options='{"mode":"datebox"}' />
+		<input name="theme3" id="theme3" type="text" data-role="datebox" data-options='{"mode":"flipbox"}' />
+		<input name="theme4" id="theme4" type="text" data-role="datebox" data-options='{"mode":"slidebox", "overrideSlideFieldOrder":["y","m","d","h","i"], "overrideDateFormat":"%Y-%m-%d %H:%M:00"}' />
+	</div>
+</body>
+</html>
---
+++ theme/sheet.php
@@ -0,0 +1,123 @@
+<?php
+header('Content-Type: text/css');
+
+$star = "*";
+$defaults = array(
+	'cgdw' => '36', // Cal box width (calbox)
+	'cgdh' => '30', // Cal box height (calbox)
+	'cgdl' => '30', // Cal box line-height (calbox)
+	'cgfs' => '12', // Cal box font size (calbox)
+	'cgdec' => '#888', // Empty date color (calbox)
+	'cgddc' => '#888', // Disabled date color (calbox)
+	'dcw' => '77', // button width (datebox)
+	'dsiw' => '74', // input width (datebox)
+	'width' => '280', // Master Width
+	'fw' => '77', // width (flipbox)
+	'fwl' => '260', // lens width (flipbox)
+	'fh' => '30', // cell heigh (flipbox)
+	'fhl' => '40', // lens height (flipbox)
+	'fch' => '125', // content height (flipbox)
+	'fcsh' => '120', // scroller height (flipbox)
+	'ssyw' => 84,
+	'ssyl' => 30,
+	'ssyh' => 30,
+	'ssyf' => 14,
+	'ssmw' => 51,
+	'ssml' => 30,
+	'ssmh' => 30,
+	'ssmf' => 12,
+	'ssdw' => 32,
+	'ssdl' => 20,
+	'ssdh' => 38,
+	'ssdf' => 14,
+	'sshw' => 32,
+	'sshl' => 22,
+	'sshh' => 24,
+	'sshf' => 14,
+	'swdf' => 10
+);
+
+$use = array();
+
+foreach ( $defaults as $key => $value ) {
+	if ( isset($_REQUEST[$key]) ) {
+		$use[$key] = $_REQUEST[$key];
+	} else {
+		$use[$key] = $value;
+	}
+}
+
+echo "/{$star} jQuery Mobile Framework : plugin to provide a date and time picker.\n";
+echo " {$star} Copyright (c) JTSage\n";
+echo " {$star} CC 3.0 Attribution.  May be relicensed without permission/notification.\n";
+echo " {$star} https://github.com/jtsage/jquery-mobile-datebox\n";
+echo " {$star}/\n";
+
+echo "\n\n/{$star} Base input element Styles {$star}/\n";
+
+echo ".ui-input-datebox { width: 97%; background-image: none; padding: .4em; line-height: 1.4; font-size: 16px; display: block; padding-top: 0px; padding-bottom: 0px; } \n";
+echo ".ui-input-datebox { min-height: 38px; } \n";
+echo ".ui-input-datebox .ui-btn-icon-notext { margin-top: 5px !important; margin-bottom: 5px !important; }\n";
+echo ".ui-input-datebox input { width: 100% !important; padding: 0 !important; margin-top: 5px !important; margin-right: -40px !important; border: 1px solid transparent !important; vertical-align: middle; display: inline-block !important; background-color: transparent; zoom: 1; *display: inline; }\n";
+echo ".ui-input-datebox input:focus { outline: none;}\n";
+echo ".ui-input-datebox .ui-btn-text {display: none;}\n\n";
+
+echo "@media all and (min-width: 450px){\n";
+echo "  .ui-input-datebox { width: 60%; display: inline-block; zoom:1; *display: inline; }\n";
+echo "}\n";
+
+echo "\n\n/{$star} Calendar Mode Styles {$star}/\n";
+echo ".ui-datebox-gridheader { text-align: center; }\n";
+echo ".ui-datebox-gridheader h4 { text-align: center; display: inline-block; margin-top: 10px; margin-bottom: 10px; zoom:1; *display: inline;}\n";
+echo ".ui-datebox-gridplus { float: right; }\n";
+echo ".ui-datebox-gridminus { float: left; }\n";
+echo ".ui-datebox-gridplus-rtl { float: left; }\n";
+echo ".ui-datebox-gridminus-rtl { float: right; }\n";
+echo ".ui-datebox-gridrow { margin-left: 5px; margin-right: 5px; }\n";
+echo ".ui-datebox-grid { clear: both; margin-bottom: 5px; }\n";
+echo ".ui-datebox-griddate { width: {$use['cgdw']}px; height: {$use['cgdh']}px; display: inline-block; vertical-align: middle; text-align: center; line-height: {$use['cgdl']}px; font-weight: bold; font-size: {$use['cgfs']}px; zoom:1; *display: inline;}\n";
+echo ".ui-datebox-griddate-empty { border: 1px solid transparent; color: {$use['cgdec']}; }\n";
+echo ".ui-datebox-griddate-label { height: 15px !important; line-height: 15px !important; color: black;}\n";
+echo ".ui-datebox-griddate-disable { color: {$use['cgddc']}; }\n";
+
+echo "\n\n/{$star} Android Mode Styles {$star}/\n";
+echo ".ui-datebox-header h4 { margin-top: 5px; margin-bottom: 5px; text-align: center; }\n";
+echo ".ui-datebox-controls { text-align: center; }\n";
+echo ".ui-datebox-controls div { width: {$use['dcw']}px; text-align: center; display: inline-block; zoom: 1; *display: inline;}\n";
+echo ".ui-datebox-scontrols { text-align: center; }\n";
+echo ".ui-datebox-scontrols div { width: 55px; text-align: center; display: inline-block; zoom: 1; *display: inline;}\n";
+echo ".ui-datebox-scontrols .ui-datebox-sinput { width: 68px; }\n";
+echo ".ui-datebox-scontrols .ui-datebox-sinput input { width: 48px; text-align: center; margin-left: 3px; }\n";
+echo ".ui-datebox-input { width: {$use['dsiw']}px !important; margin-left: 1px; margin-right: 1px; text-align: center !important; display: inline-block !important; zoom:1; *display: inline; }\n";
+
+echo "\n\n/{$star} Slide Mode Styles {$star}/\n";
+echo ".ui-datebox-slide { width: {$use['width']}px; margin-left: auto; margin-right: auto;}\n";
+echo ".ui-datebox-sliderow-d { margin-bottom: 5px; text-align: center; height: 40px; width: {$use['width']}px; overflow: hidden;}\n";
+echo ".ui-datebox-sliderow-ym { margin-bottom: 5px; text-align: center; height: 32px; width: {$use['width']}px; overflow: hidden;}\n";
+echo ".ui-datebox-sliderow-hi { text-align: center; height: 32px; width: {$use['width']}px; overflow: hidden;}\n";
+echo ".ui-datebox-sliderow-int { display: inline-block; white-space: nowrap;}\n";
+echo ".ui-datebox-slideyear { text-align: center; display: inline-block; zoom:1; *display:inline; width: {$use['ssyw']}px; vertical-align: middle; line-height: {$use['ssyl']}px; height: {$use['ssyh']}px; font-size: {$use['ssyf']}px; font-weight: bold; }\n";
+echo ".ui-datebox-slidemonth { text-align: center; display: inline-block; zoom:1; *display:inline; width: {$use['ssmw']}px; vertical-align: middle; line-height: {$use['ssml']}px; height: {$use['ssmh']}px; font-size: {$use['ssmf']}px; font-weight: bold; }\n";
+echo ".ui-datebox-slideday { text-align: center; display: inline-block; zoom:1; *display:inline; width: {$use['ssdw']}px; vertical-align: middle; line-height: {$use['ssdl']}px; height: {$use['ssdh']}px; font-size: {$use['ssdf']}px; font-weight: bold; }\n";
+echo ".ui-datebox-slidehour { text-align: center; display: inline-block; zoom:1; *display:inline; width: {$use['sshw']}px; vertical-align: middle; line-height: {$use['sshl']}px; height: {$use['sshh']}px; font-size: {$use['sshf']}px; font-weight: bold; }\n";
+echo ".ui-datebox-slidemins { text-align: center; display: inline-block; zoom:1; *display:inline; width: {$use['sshw']}px; vertical-align: middle; line-height: {$use['sshl']}px; height: {$use['sshh']}px; font-size: {$use['sshf']}px; font-weight: bold; }\n";
+echo ".ui-datebox-slidearrow { text-align: center; display: inline-block; zoom:1; *display:inline; width: 10px; vertical-align: middle; line-height: 38px; height: 38px; font-size: 10px; font-weight: bold; }\n";
+echo ".ui-datebox-slidewday { font-size: {$use['swdf']}px; font-weight: normal; }\n";
+
+echo "\n\n/{$star} Flip Mode Styles {$star}/\n";
+echo ".ui-datebox-flipcontent { text-align: center; height: {$use['fch']}px; margin-bottom: -{$use['fhl']}px;}\n";
+echo ".ui-datebox-flipcontent div { margin-left: 3px; margin-right: 3px; width: {$use['fw']}px; height: {$use['fcsh']}px; display: inline-block; text-align: center; zoom: 1; *display: inline; overflow: hidden;}\n";
+echo ".ui-datebox-flipcenter { border: 1px solid #eee; height: ${use['fhl']}px; margin-left: 10px; width: {$use['fwl']}px; margin-right: auto; margin-left: auto; position: relative; top: -45px;}\n";
+echo ".ui-datebox-flipcontent ul { list-style-type: none; display: inline; }\n";
+echo ".ui-datebox-flipcontent li { height: {$use['fh']}px; }\n";
+echo ".ui-datebox-flipcontent li span { margin-top: 7px; display: block; }\n";
+
+echo "\n\n/{$star} Shared Styles {$star}/\n";
+echo ".ui-datebox-container { border: 5px solid #111 !important; width: {$use['width']}px; }\n";
+echo ".ui-datebox-screen { position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; }\n";
+echo ".ui-datebox-screen-modal { background-color: black; -moz-opacity: 0.8; opacity:.80; filter: alpha(opacity=80); }\n";
+echo ".ui-datebox-hidden { display: none; }\n";
+echo ".ui-dialog .ui-datebox-container { border: none !important; }\n";
+echo ".ui-datebox-collapse a { display: inline-block; width: 45% }\n";
+echo "\n.ui-datebox-inline { margin-top: 5px; border: 5px solid #111111 !important; margin-left: auto; margin-right: auto; text-align: center; }\n";
+echo ".ui-datebox-inlineblind { margin-top: 5px; border: 5px solid #111111 !important; margin-left: auto; margin-right: auto; text-align: center; }\n";
