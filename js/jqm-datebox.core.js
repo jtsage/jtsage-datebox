48f0a3c41397b7778b10b7eda187a175c0a916b2
J.T. Sage <jtsage+github@gmail.com>
2012-12-06 13:04:52 -0500

    Allow periods in control names (or rather, don't choke on it) - closes #234

--- js/jqm-datebox.core.js
+++ js/jqm-datebox.core.js
@@ -1136,8 +1136,8 @@
 			if ( typeof o.overrideDialogLabel === 'undefined' ) {
 				if ( typeof w.d.input.attr('placeholder') !== 'undefined' ) { return w.d.input.attr('placeholder'); }
 				if ( typeof w.d.input.attr('title') !== 'undefined' ) { return w.d.input.attr('title'); }
-				if ( w.d.wrap.parent().find('label[for='+w.d.input.attr('id')+']').text() !== '' ) {
-					return w.d.wrap.parent().find('label[for='+w.d.input.attr('id')+']').text();
+				if ( w.d.wrap.parent().find('label[for=\''+w.d.input.attr('id')+'\']').text() !== '' ) {
+					return w.d.wrap.parent().find('label[for=\''+w.d.input.attr('id')+'\']').text();
 				}
 				return false;
 			}
