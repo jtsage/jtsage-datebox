560e7e4ea6d1cec9b3ba1b9900233d14d009424d
J.T. Sage <jtsage+github@gmail.com>
2012-12-06 13:10:20 -0500

    Version bump - jqm1.2.0

--- js/jqm-datebox.core.js
+++ js/jqm-datebox.core.js
@@ -10,7 +10,7 @@
 	$.widget( "mobile.datebox", $.mobile.widget, {
 		options: {
 			// All widget options, including some internal runtime details
-			version: '2-1.1.0-2012091200', // jQMMajor.jQMMinor.DBoxMinor-YrMoDaySerial
+			version: '2-1.2.0-2012120600', // jQMMajor.jQMMinor.DBoxMinor-YrMoDaySerial
 			theme: false,
 			themeDefault: 'c',
 			themeHeader: 'a',
