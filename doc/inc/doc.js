d71de24da620bbcba848813257b09545402a3032
JTSage <jtsage+datebox@gmail.com>
2014-07-27 17:37:15 -0400

    Doc: "default values", "data limiting"

--- doc/inc/doc.js
+++ doc/inc/doc.js
@@ -2,10 +2,20 @@ $(document).on('change', '.demopick', function(e){
 	thisSel = $(e.currentTarget);
 	thisBox = '#'+thisSel.data('link');
 	thisVal = thisSel.val();
+	thisJSON = makeJSON(thisVal);
 	if ( thisVal === "true" ) { thisVal = true; }
 	if ( thisVal === "false" ) { thisVal = false; }
 	if ( thisVal == parseInt(thisVal,10) ) { thisVal = parseInt(thisVal,10); }
+	if ( thisJSON !==false ) { thisVal = thisJSON; }
 	thisObj = {}; thisObj[thisSel.data('opt')] = thisVal;
 	$(thisBox).datebox(thisObj);
 	$(thisBox).datebox('refresh');
 })
+
+function makeJSON(str) {
+	try {
+		return jQuery.parseJSON(str);
+	} catch (e) {
+		return false;
+	}
+}
