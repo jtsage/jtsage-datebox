d2ecf31e9bed44b77cc80fd71e8f861d54624ed4
JTSage <jtsage+datebox@gmail.com>
2014-07-27 21:26:43 -0400

    Doc: 3rd party packages

--- doc/8-pack.php
+++ doc/8-pack.php
@@ -1,11 +1,17 @@
 <?php
 require_once('inc/func.php');
-echo do_header("3rd-Party Integration", array('7-demos.php',"Advanced Demos"), NULL);
+echo do_header("3rd-Party Integration", array('7-demos.php',"Advanced Demos"), NULL, false);
 ?>
 
+<h1>YepNope.js</h1>
+<p>YepNope used to be supported.  It is not supported any longer, as the package itself has been depreciated.  If anyone wants to make it work with the new version, I'd be happy to post it here.</p>
+<p>Documentation at the time of last working is available in the <a href="https://github.com/jtsage/jquery-mobile-datebox/commit/37d1b38c7a5caa2504d62c7a58821cbdfc121c2e">GitHub Archive</a></p>
 
+<h1>Ruby on Rails</h1>
+<p>There is a gem created by Guidecase available on <a href="https://github.com/Guidecase/jquery_mobile_datebox-rails">GitHub</a></p>
 
-
+<h1>require.js / backbone.js</h1>
+<p>Operation with this pair of packages is supported.  Please take a look at the <a href="3rd/backbonerequire/">demo</a>.</p>
 
 <?php
 echo do_footer();
