41dca4c3d9048b91c83d4505f3793d1fc9e74400
JTSage <jtsage+datebox@gmail.com>
2014-08-13 16:19:21 -0400

    1.5.0-pre/Grunt - begin grunt work.  heh.

--- js/jqm-datebox.mode.slidebox.js
+++ js/jqm-datebox.mode.slidebox.js
@@ -1,7 +1,6 @@
 /* 
  * jQuery Mobile Framework : plugin to provide a date and time picker.
  * Copyright (c) JTSage
- * CC 3.0 Attribution.  May be relicensed without permission/notification.
  * https://github.com/jtsage/jquery-mobile-datebox
  */
 /* SLIDEBOX Mode */
