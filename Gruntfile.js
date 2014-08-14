be113cd0d979cfb779ea14cbed864acf0f9c19bc
JTSage <jtsage+datebox@gmail.com>
2014-08-13 22:34:55 -0400

    1.5.0-pre/grunt - Finish out main build system.  Next is i18n build system.  Re: #331

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,32 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON( "package.json" ),
-
+		txt: {
+			copyYear: grunt.template.today( "UTC:yyyy" ),
+			banner : {
+				long: [
+					"/*",
+					" * jQuery-Mobile-DateBox <%= version %>",
+					" * Date: " + grunt.template.today( "UTC:ddd mmm d yyyy HH:MM:ss Z" ),
+					" * http://dev.jtsage.com/jQM-DateBox/",
+					" * https://github.com/jtsage/jquery-mobile-datebox",
+					" *",
+					" * Copyright 2010, <%= txt.copyYear %> JTSage. and other contributors",
+					" * Released under the MIT license.",
+					" * https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt",
+					" *",
+					" */",
+					"" ].join( grunt.util.linefeed ),
+				short: "/*! jQuery-Mobile-DateBox <%= version %> |" + 
+					grunt.template.today( "UTC:yyyy-mm-dd" ) + "T" + 
+					grunt.template.today( "UTC:HH:MM:ss" ) +
+					"Z | (c) 2010,  <%= txt.copyYear %> JTSage | " +
+					"https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt */\n"
+			}
+		},
+		qunit: {
+			files: ["docs/qunit/_cver.html"]
+		},
 		jshint: {
 			js: {
 				files: {
@@ -28,13 +53,233 @@ module.exports = function(grunt) {
 					jshintrc: ".jshintrc"
 				}
 			}
+		},
+		clean: {
+			latest: ["dist/latest/"],
+			release: ["dist/<%= pkg.version %>/"]
+		},
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: "<%= txt.banner.long %>",
+			},
+			lat_main: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.*.js"
+				],
+				dest: "dist/latest/jqm-datebox.all.js"
+			},
+			ver_main: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.*.js",
+						"!js/jqm-datebox.mode.customflip.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.all.js"
+			},
+			ver_extra: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.*.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.allextra.js"
+			},
+			ver_comp_datebox: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.datebox.js",
+						"js/jqm-datebox.mode.durationbox.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.comp.datebox.js"
+			},
+			ver_comp_calbox: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.calbox.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.comp.calbox.js"
+			},
+			ver_comp_flipbox: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.flipbox.js",
+						"js/jqm-datebox.mode.durationflipbox.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.comp.flipbox.js"
+			},
+			ver_comp_slidebox: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.slidebox.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.comp.slidebox.js"
+			},
+			ver_comp_customflip: {
+				src: [
+						"js/jqm-datebox.core.js",
+						"js/jqm-datebox.mode.customflip.js"
+				],
+				dest: "dist/<%= pkg.version %>/jqm-datebox-<%= pkg.version %>.comp.customflip.js"
+			}
+		},
+		copy: {
+			release: {
+				options: {
+					process: function (content) {
+						var texty = grunt.config("txt");
+						return content.replace(
+							/^...jQuery-Mobile-DateBox.../,
+							texty.banner.long
+						);
+					}
+				},
+				files: [ {
+					expand: true,
+					cwd: "js/",
+					src: ["*.js"],
+					dest: "dist/<%= pkg.version %>/",
+					rename: function(dest, src) {
+						return dest + src.replace(/^jqm-datebox/, "jqm-datebox-<%= pkg.version %>");
+					}
+				} ]
+			},
+			latest: {
+				options: {
+					process: function (content) {
+						var texty = grunt.config("txt");
+						return content.replace(
+							/^...jQuery-Mobile-DateBox.../,
+							texty.banner.long
+						);
+					}
+				},
+				files: [ {
+					expand: true,
+					cwd: "js/",
+					src: ["*.js"],
+					dest: "dist/latest/",
+				} ]
+			},
+			latest_css: {
+				options: {
+					process: function (content) {
+						var texty = grunt.config("txt");
+						return content.replace(
+							/^...jQuery-Mobile-DateBox.../,
+							texty.banner.long
+						);
+					}
+				},
+				files: [ {
+					expand: true,
+					cwd: "css/",
+					src: ["*.css"],
+					dest: "dist/latest/",
+				} ]
+			},
+			release_css: {
+				options: {
+					process: function (content) {
+						var texty = grunt.config("txt");
+						return content.replace(
+							/^...jQuery-Mobile-DateBox.../,
+							texty.banner.long
+						);
+					}
+				},
+				files: [ {
+					expand: true,
+					cwd: "css/",
+					src: ["*.css"],
+					dest: "dist/<%= pkg.version %>/",
+					rename: function(dest, src) {
+						return dest + src.replace(/^jqm-datebox/, "jqm-datebox-<%= pkg.version %>");
+					}
+				} ]
+			}
+		},
+		uglify: {
+			options: {
+				banner: "<%= txt.banner.short %>",
+				verbose: true
+			},
+			release: {
+				files: [ {
+					expand: true,
+					src: ["dist/<%= pkg.version %>/*.js"],
+					dest: "",
+					ext: ".min.js",
+					extDot: "last" 
+				} ]
+			},
+			latest: {
+				files: [ {
+					expand: true,
+					src: ["dist/latest/*.js"],
+					dest: "",
+					ext: ".min.js",
+					extDot: "last" 
+				} ]
+			}
+		},
+		cssmin: {
+			release: {
+				options: {
+					banner: "<%= txt.banner.short %>"
+				},
+				files: {
+					"dist/<%= pkg.version %>/jqm-datebox<%= pkg.version %>.min.css": [
+						"css/jqm-datebox.css"]
+				}
+			},
+			latest: {
+				options: {
+					banner: "<%= txt.banner.short %>"
+				},
+				files: {
+					"dist/latest/jqm-datebox.min.css": [
+						"css/jqm-datebox.css"]
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-contrib-qunit" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-contrib-copy" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-cssmin");
+	
+	grunt.registerTask( "release", [
+		"clean:release",
+		"concat:ver_main",
+		"concat:ver_extra",
+		"concat:ver_comp_datebox",
+		"concat:ver_comp_calbox",
+		"concat:ver_comp_flipbox",
+		"concat:ver_comp_slidebox",
+		"concat:ver_comp_customflip",
+		"copy:release",
+		"copy:release_css",
+		"uglify:release",
+		"cssmin:release"
+	]);
+	
+	grunt.registerTask( "latest", [
+		"clean:latest",
+		"concat:lat_main",
+		"copy:latest",
+		"copy:latest_css",
+		"uglify:latest",
+		"cssmin:latest"
+	]);
 
-	grunt.registerTask( "test", [ "jshint" ]);
+	grunt.registerTask( "test", ["jshint", "qunit"] );
 
-	grunt.registerTask( "default", [ "jshint" ]);
+	grunt.registerTask( "default", [ "jshint", "qunit", "latest" ]);
+	
 
 };
--- docs/js/doc.js
+++ docs/js/doc.js
@@ -1,17 +1,17 @@
-$(document).on('change', '.demopick', function(e){
+$(document).on("change", ".demopick", function(e){
 	var	thisObj, 
 		thisSel = $(e.currentTarget),
-		thisBox = '#'+thisSel.data('link'),
+		thisBox = "#"+thisSel.data("link"),
 		thisVal = thisSel.val(),
 		thisJSON = makeJSON(thisVal);
 		
 	if ( thisVal === "true" ) { thisVal = true; }
 	if ( thisVal === "false" ) { thisVal = false; }
-	if ( thisVal == parseInt(thisVal,10) ) { thisVal = parseInt(thisVal,10); }
+	if ( thisVal === parseInt(thisVal,10) ) { thisVal = parseInt(thisVal,10); }
 	if ( thisJSON !==false ) { thisVal = thisJSON; }
-	thisObj = {}; thisObj[thisSel.data('opt')] = thisVal;
+	thisObj = {}; thisObj[thisSel.data("opt")] = thisVal;
 	$(thisBox).datebox(thisObj);
-	$(thisBox).datebox('refresh');
+	$(thisBox).datebox("refresh");
 });
 
 function makeJSON(str) {
@@ -22,24 +22,25 @@ function makeJSON(str) {
 	}
 }
 var langs = [{"data":[
-	"en: English US", "af: Afrikaans", "ar: العربية", "bg: български език", "ca: Català", "cs: Čeština",
-	"da: Dansk", "de: Deutsch", "el: ελληνικά", "es-ES: Español", "fi: Suomi", "fr: Français", "he: עברית",
-	"hr: Hrvatski Jezik", "hu: Magyar", "id: Bahasa Indonesia", "it: Italiano", "ja: 日本語", "ko: 한국어",
-	"lt: Lietuvių Kalba", "nl: Nederlands", "nl-BE: Nederlands	, Belgium", "no: Norsk", "pl: Język Polski",
-	"pt-BR: Português", "pt-PT: Português", "ro: Limba Română", "ru: русский язык", "sl: Slovenski Jezik",
-	"sr: српски језик", "sv-SE: Svenska", "th: ไทย", "tr: Türkçe", "uk: українська мова", "vi: Tiếng Việt",
-	"zh-CN: 中文 (Simplified)", "zh-TW: 中文 (Traditional)"]}];
+	"en: English US", "af: Afrikaans", "ar: العربية", "bg: български език", "ca: Català",
+	"cs: Čeština", "da: Dansk", "de: Deutsch", "el: ελληνικά", "es-ES: Español", "fi: Suomi",
+	"fr: Français", "he: עברית", "hr: Hrvatski Jezik", "hu: Magyar", "id: Bahasa Indonesia",
+	"it: Italiano", "ja: 日本語", "ko: 한국어", "lt: Lietuvių Kalba", "nl: Nederlands",
+	"nl-BE: Nederlands, Belgium", "no: Norsk", "pl: Język Polski", "pt-BR: Português",
+	"pt-PT: Português", "ro: Limba Română", "ru: русский язык", "sl: Slovenski Jezik",
+	"sr: српски језик", "sv-SE: Svenska", "th: ไทย", "tr: Türkçe", "uk: українська мова",
+	"vi: Tiếng Việt", "zh-CN: 中文 (Simplified)", "zh-TW: 中文 (Traditional)"]}];
 		
-var changeLang = function(a) {
+window.changeLang = function(a) {
 	var thisLangRaw = langs[0].data[a.custom[0]],
 		colonPos = thisLangRaw.search(/:/),
 		thisLang = thisLangRaw.substr(0,colonPos);
 		
 	$( "[data-role='datebox']" ).each(function() {
-		if ( typeof $(this).data('mobile-datebox') !== "undefined" ) {
-			$(this).datebox({'useLang': thisLang});
+		if ( typeof $(this).data("mobile-datebox") !== "undefined" ) {
+			$(this).datebox({"useLang": thisLang});
 		} else {
-			$(this).attr('datebox-use-lang', thisLang);
+			$(this).attr("datebox-use-lang", thisLang);
 		}
 	});
 };
