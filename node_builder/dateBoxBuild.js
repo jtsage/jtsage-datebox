f6e13034a22d27925857a417a3feaf9171036822
JTSage <jtsage+datebox@gmail.com>
2015-10-30 11:55:34 -0400

    Get closer to having a working builder

--- node_builder/dateBoxBuild.js
+++ node_builder/dateBoxBuild.js
@@ -1,16 +1,28 @@
 var fs = require('fs'),
 	m = require('module'),
 	UglifyJS = require("uglify-js"),
-	pretty = require('js-object-pretty-print').pretty;
+	pretty = require('js-object-pretty-print').pretty,
+	execSync = require('exec-sync'),
+	cssmin = require('cssmin'),
+	admzip = require('adm-zip'),
+	crypto = require('crypto');
 
-var path = process.argv[2],
+var today = new Date(),
+	path = process.argv[2],
 	framework = process.argv[3],
-	modes = [];
+	modes = [],
+	cssFile = "sheet.php";
+
+if ( process.argv[3] === "bootstrap" ) {
+	cssFile = "sheet-bootstrap.php";
+}
 
 if ( typeof process.argv[4] === "undefined" ) {
 	modes = [
 		"datebox",
-		"flipbox"
+		"flipbox",
+		"slidebox",
+		"calbox"
 	];
 } else {
 	for ( var i = 4; i < process.argv.length; i++ ) {
@@ -75,17 +87,37 @@ var code = ast.print(stream);
 
 var preamble = [
 	"/*",
-	" * JTSage-DateBox-",
-	" * For: ",
-	" * Date: ",
+	" * JTSage-DateBox",
+	" * For: " + framework,
+	" * Date: " + today.toJSON(),
 	" * http://dev.jtsage.com/DateBox/",
 	" * https://github.com/jtsage/jquery-mobile-datebox",
 	" *",
-	" * Copyright 2010, <%= txt.copyYear %> JTSage. and other contributors",
+	" * Copyright 2010, " + today.getFullYear() + " JTSage. and other contributors",
 	" * Released under the MIT license.",
 	" * https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt",
 	" *",
 	" */" ].join("\n");
 
-console.log(preamble + "\n\n" + stream.toString());
+var finished_uncomp = preamble + "\n\n" + stream.toString();
+var finished_comp = UglifyJS.minify(finished_uncomp, {fromString: true, outSourceMap: "jtsage-datebox.js.map"});
+var finished_css_uncomp = execSync("/usr/bin/php " + path + "../css/" + cssFile);
+var finished_css_comp = cssmin(finished_css_uncomp);
+
+var zip = new admzip();
+zip.addFile('jtsage-datebox/js/jtsage-datebox.js', new Buffer(finished_uncomp));
+zip.addFile('jtsage-datebox/js/jtsage-datebox.min.js', new Buffer(finished_comp.code));
+zip.addFile('jtsage-datebox/js/jtsage-datebox.js.map', new Buffer(finished_comp.map));
+zip.addFile('jtsage-datebox/css/jtsage-datebox.css', new Buffer(finished_css_uncomp));
+zip.addFile('jtsage-datebox/css/jtsage-datebox.min.css', new Buffer(finished_css_comp));
+
+var zipData = zip.toBuffer();
+
+var zipFile = "./output/jtsDateBox-" + crypto.randomBytes(Math.ceil(6)).toString('hex').slice(0,12) + ".zip"
+
+zip.writeZip(zipFile);
+
+
+//console.log(zipFile);
+//console.log(preamble + "\n\n" + stream.toString());
 
