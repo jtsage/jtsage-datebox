42daf4eeac2e279007d1be687255218ab17722fd
JTSage <jtsage+datebox@gmail.com>
2019-03-09 03:43:21 -0500

    Clean up un-needed devel dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 					" *",
 					" * Copyright 2010, <%= txt.copyYear %> JTSage. and other contributors",
 					" * Released under the MIT license.",
-					" * https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt",
+					" * https://github.com/jtsage/jtsage-datebox/blob/master/LICENSE.txt",
 					" *",
 					" */",
 					"" ].join( grunt.util.linefeed ),
@@ -25,11 +25,8 @@ module.exports = function(grunt) {
 					grunt.template.today( "UTC:yyyy-mm-dd" ) + "T" + 
 					grunt.template.today( "UTC:HH:MM:ss" ) +
 					"Z | (c) 2010,  <%= txt.copyYear %> JTSage | " +
-					"https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt */\n"
+					"https://github.com/jtsage/jtsage-datebox/blob/master/LICENSE.txt */\n"
 			}
-		},
-		qunit: {
-			files: ["docs/qunit/_cver.html"]
 		},
 		jshint: {
 			js: {
@@ -78,17 +75,6 @@ module.exports = function(grunt) {
 					"indent": 4,
 					"node": true,
 					"predef": [ "jQuery", "document", "window", "setTimeout", "clearTimeout" ]
-				}
-			},
-			doc: {
-				files: {
-					src: [
-						"docs/js/*.js",
-						"docs/qunit/*.js"
-					]
-				},
-				options: {
-					jshintrc: "docs/qunit/.jshintrc"
 				}
 			},
 			grunt: {
@@ -143,32 +129,6 @@ module.exports = function(grunt) {
 				} ]
 			}
 		},
-		cssmin: {
-			release: {
-				options: {
-					banner: "<%= txt.banner.short %>"
-				},
-				files: [{
-      				expand: true,
-      				src: [ "dist/<%= pkg.version %>/*.css", "!dist/<%= pkg.version %>/*.min.css" ],
-      				dest: "",
-      				extDot: "last",
-      				ext: ".min.css"
-    			}]
-			},
-			latest: {
-				options: {
-					banner: "<%= txt.banner.short %>"
-				},
-				files: [{
-      				expand: true,
-      				src: [ "dist/latest/*.css", "!dist/latest/*.min.css"],
-      				dest: "",
-      				extDot: "last",
-      				ext: ".min.css"
-    			}]
-			}
-		},
 		committers: {
 			options: {
 				sort: "commits",
@@ -181,23 +141,7 @@ module.exports = function(grunt) {
 				src: [ "i18n/locale/*/datebox.po"]
 			}
 		},
-		jekyll: {
-			options: {
-				src : "docs/",
-				dest: "docs/_site"
-			},
-			latest: {
-				options: {
-					config: "docs/_config.yml,docs/_config.dev.yml"
-				}
-			},
-			release: {
-				options: {
-					config: "docs/_config.yml"
-				}
-			}
-		},
-  		buildDBox: {
+		buildDBox: {
 			latest_bootstrap: {
 				options: {
 					dest: "dist/latest/jtsage-datebox.bootstrap.js",
@@ -246,26 +190,12 @@ module.exports = function(grunt) {
 					],
 				}]
 			},
-			latest_jqm: {
-				options: {
-					dest: "dist/latest/jtsage-datebox.jqm.js",
-					includeBinding: true,
-				},
-				files: [{
-					expand: true,
-					src: [
-						"src/js/baseObject.js",
-						"src/js/framework/jqm.js",
-						"src/js/lib/*.js",
-						"src/js/modes/*.js"
-					],
-				}]
-			},
 			main_bootstrap: {
 				options: {
 					dest: "dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.bootstrap.js",
 					includeBinding: true,
 				},
+				libFiles: [ "src/js/external/*.js" ],
 				files: [{
 					expand: true,
 					src: [
@@ -281,6 +211,7 @@ module.exports = function(grunt) {
 					dest: "dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.bootstrap4.js",
 					includeBinding: true,
 				},
+				libFiles: [ "src/js/external/*.js" ],
 				files: [{
 					expand: true,
 					src: [
@@ -296,6 +227,7 @@ module.exports = function(grunt) {
 					dest: "dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.jqueryui.js",
 					includeBinding: true,
 				},
+				libFiles: [ "src/js/external/*.js" ],
 				files: [{
 					expand: true,
 					src: [
@@ -306,63 +238,24 @@ module.exports = function(grunt) {
 					],
 				}]
 			},
-			main_jqm: {
-				options: {
-					dest: "dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.jqm.js",
-					includeBinding: true,
-				},
-				files: [{
-					expand: true,
-					src: [
-						"src/js/baseObject.js",
-						"src/js/framework/jqm.js",
-						"src/js/lib/*.js",
-						"src/js/modes/*.js"
-					],
-				}]
 			},
-  		},
-  		copy: {
-  			web1: {
-  				src: "src/css/sheet-bootstrap.php",
-  				dest: "docs/theme/bootstrap/sheet.php"
-  			},
-      		web2: {
-      			src: "src/css/sheet.php",
-      			dest: "docs/theme/jqm/sheet.php"
-      		},
-      		web3: {
-      			src: "src/css/sheet-jqueryui.php",
-      			dest: "docs/theme/jqueryui/sheet.php"
-      		},
-      		web4: {
-  				src: "src/css/sheet-bootstrap4.php",
-  				dest: "docs/theme/bootstrap4/sheet.php"
-  			},
-      		cssfix: {
-      			files: [
-      				{ src: "src/css/sheet-bootstrap.php",	dest: "docs/_site/theme/bootstrap/sheet.php" },
-      				{ src: "src/css/sheet-bootstrap4.php",	dest: "docs/_site/theme/bootstrap/sheet4.php" },
-      				{ src: "src/css/sheet.php", 			dest: "docs/_site/theme/jqm/sheet.php" },
-  					{ src: "src/css/sheet-jqueryui.php", 	dest: "docs/_site/theme/jqueryui/sheet.php" }
-      			]
-      		},
-      		builder1: {
-      			expand: true,
-      			cwd: "src/js",
-      			src: "**/*.js",
-      			dest: "node_builder/src/<%= pkg.version %>/js/"
-      		},
-      		builder2: {
-      			expand: true,
-      			cwd: "dist/<%= pkg.version %>/",
-      			src: "*.css",
-      			dest: "node_builder/src/<%= pkg.version %>/css/",
-      			rename: function(dest, src) {
-        			return dest + src.replace( "jtsage-datebox-" + pkgJSON.version + "." , "" );
-      			}
-      		}
-  		},
+		copy: {
+			builder1: {
+				expand: true,
+				cwd: "src/js",
+				src: "**/*.js",
+				dest: "node_builder/src/<%= pkg.version %>/js/"
+			},
+			builder2: {
+				expand: true,
+				cwd: "dist/<%= pkg.version %>/",
+				src: "*.css",
+				dest: "node_builder/src/<%= pkg.version %>/css/",
+				rename: function(dest, src) {
+					return dest + src.replace( "jtsage-datebox-" + pkgJSON.version + "." , "" );
+				}
+			}
+		},
 		prettify: {
 			options: {
 				// Task-specific options go here.
@@ -375,50 +268,14 @@ module.exports = function(grunt) {
 				dest: "docs/_site/"
 			},
 		},
-		htmllint: {
-			all: ["docs/_site/**/*.html"]
-		},
-		exec: {
-			latest_make_css_jqm: {
-				command: "/usr/bin/php src/css/sheet.php > dist/latest/jtsage-datebox.jqm.css"
-			},
-			latest_make_css_bootstrap: {
-				command: "/usr/bin/php src/css/sheet-bootstrap.php > dist/latest/jtsage-datebox.bootstrap.css"
-			},
-			latest_make_css_bootstrap4: {
-				command: "/usr/bin/php src/css/sheet-bootstrap4.php > dist/latest/jtsage-datebox.bootstrap4.css"
-			},
-			latest_make_css_jqueryui: {
-				command: "/usr/bin/php src/css/sheet-jqueryui.php > dist/latest/jtsage-datebox.jqueryui.css"
-			},
-			main_make_css_jqm: {
-				command: "/usr/bin/php src/css/sheet.php > dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.jqm.css"
-			},
-			main_make_css_bootstrap: {
-				command: "/usr/bin/php src/css/sheet-bootstrap.php > dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.bootstrap.css"
-			},
-			main_make_css_bootstrap4: {
-				command: "/usr/bin/php src/css/sheet-bootstrap4.php > dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.bootstrap4.css"
-			},
-			main_make_css_jqueryui: {
-				command: "/usr/bin/php src/css/sheet-jqueryui.php > dist/<%= pkg.version %>/jtsage-datebox-<%= pkg.version %>.jqueryui.css"
-			}
-
-		}
 	});
 
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
-	grunt.loadNpmTasks( "grunt-contrib-qunit" );
 	grunt.loadNpmTasks( "grunt-contrib-clean" );
 	grunt.loadNpmTasks( "grunt-contrib-uglify" );
-	grunt.loadNpmTasks( "grunt-contrib-cssmin" );
 	grunt.loadNpmTasks( "grunt-contrib-copy" );
 	grunt.loadNpmTasks( "grunt-git-committers" );
-	grunt.loadNpmTasks( "grunt-jekyll" );
-	grunt.loadNpmTasks( "grunt-contrib-watch" );
 	grunt.loadNpmTasks( "grunt-prettify" );
-	grunt.loadNpmTasks( "grunt-html" );
-	grunt.loadNpmTasks( "grunt-exec" );
 	
 	grunt.task.loadTasks( "build/tasks" );
 	
@@ -432,85 +289,30 @@ module.exports = function(grunt) {
 	grunt.registerTask( "jshint_sane", "Run jsHint with sane values", [
 		"jshint:js_sane",
 	]);
-		
-	grunt.registerTask( "cssfix", "Quick Build a CSS fix", [
-		"exec:latest_make_css_jqm",
-		"exec:latest_make_css_bootstrap",
-		"exec:latest_make_css_bootstrap4",
-		"exec:latest_make_css_jqueryui",
-		"exec:main_make_css_jqm",
-		"exec:main_make_css_bootstrap",
-		"exec:main_make_css_bootstrap4",
-		"exec:main_make_css_jqueryui",
-		"cssmin:latest",
-		"cssmin:release",
-		"copy:web1",
-		"copy:web2",
-		"copy:web3",
-		"copy:web4",
-		"copy:cssfix"
-	] );
+
 
 	grunt.registerTask( "release", "Build a release version of DateBox", [
 		"jshint:js",
 		"jshint:js2",
-		//"qunit",
 		"clean:release",
 		"clean:web",
 		"buildDBox:main_jqm",
 		"buildDBox:main_bootstrap",
 		"buildDBox:main_bootstrap4",
-		"buildDBox:main_jqueryui",
-		"exec:main_make_css_jqm",
-		"exec:main_make_css_bootstrap",
-		"exec:main_make_css_bootstrap4",
-		"exec:main_make_css_jqueryui",
 		"uglify:release",
-		"cssmin:release",
 		"committers",
-		"copy:web1",
-		"copy:web2",
-		"copy:web3",
-		"copy:web4",
 		"updatebuilder",
-		"jekyll:release",
 		"prettify",
 		"makei18n"
 	] );
 
-	grunt.registerTask( "bugrelease", "Build a bug release version of DateBox", [
-		"jshint:js",
-		"jshint:js2",
-		//"qunit",
-		"clean:release",
-		"buildDBox:main_jqm",
-		"buildDBox:main_bootstrap",
-		"buildDBox:main_bootstrap4",
-		"buildDBox:main_jqueryui",
-		"exec:main_make_css_jqm",
-		"exec:main_make_css_bootstrap",
-		"exec:main_make_css_bootstrap4",
-		"exec:main_make_css_jqueryui",
-		"uglify:release",
-		"cssmin:release",
-		"copy:cssfix",
-		"updatebuilder"
-	] );
-	
 	grunt.registerTask( "latest", "Build a working version of DateBox (no testing)", [
 		"clean:latest",
-		//"buildDBox:latest_jqm",
 		"buildDBox:latest_bootstrap",
 		"buildDBox:latest_bootstrap4",
 		"buildDBox:latest_jqueryui",
-		// "exec:latest_make_css_jqm",
-		// "exec:latest_make_css_bootstrap",
-		// "exec:latest_make_css_bootstrap4",
-		// "exec:latest_make_css_jqueryui",
 		"uglify:latest",
-		//"cssmin:latest",
 	]);
-	
 
 	grunt.registerTask( "i18n", "Build the i18n files", [
 		"clean:i18n",
@@ -518,13 +320,15 @@ module.exports = function(grunt) {
 		"uglify:i18n"
 	] );
 
-	grunt.registerTask( "updatebuilder", "Update web builder sources", [ "clean:builder", "copy:builder1", "copy:builder2" ] );
+	grunt.registerTask( "updatebuilder", "Update web builder sources", [ 
+		"clean:builder",
+		"copy:builder1",
+		"copy:builder2"
+	] );
 
 	grunt.registerTask( "web", "Build the documentation site", [
-		"clean:web", "copy:web1", "copy:web2", "copy:web3", "copy:web4", "jekyll:release", "prettify"
+		"clean:web", "prettify"
 	] );
-	grunt.registerTask( "testweb", "Build the documentation site quickly", ["clean:web", "copy:web1", "copy:web2", "copy:web3", "copy:web4", "jekyll:release"] );
-	grunt.registerTask( "devweb", "Test the documentation site", ["copy:web1", "copy:web2", "copy:web3", "copy:web4",  "jekyll:latest", "prettify"] );
 	grunt.registerTask( "fulltest", "Deeply test the DateBox Suite", [ "jshint_reg"] );
 	grunt.registerTask( "test", "Test the DateBox Suite", ["jshint:js", "jshint:js2"] );
 
