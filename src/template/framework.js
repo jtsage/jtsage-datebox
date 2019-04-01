d9c469fe0039ff93693ee259b5a8768b6d879236
J.T.Sage <jtsage+datebox@gmail.com>
2019-03-31 20:12:57 -0400

    Add template for frameworks. #457

---
+++ src/template/framework.js
@@ -0,0 +1,472 @@
+/**
+ * JTSage-DateBox
+ * @fileOverview FRAMEWORK v4 Themes and StyleFunctions
+ * This file supports: datebox, flipbox, slidebox, calbox.
+ * 
+ * calbox: <grade>
+ * datebox: <grade>
+ * slidebox: <grade>
+ * flipbox: <grade>
+ * 
+ * This file is the template for these!
+ * 
+ * @author J.T.Sage <jtsage+datebox@gmail.com>
+ * @author {@link https://github.com/jtsage/jtsage-datebox/contributors|GitHub Contributors}
+ * @license {@link https://github.com/jtsage/jtsage-datebox/blob/master/LICENSE.txt|MIT}
+ * @version 5.0.0
+ */
+
+mergeOpts({
+	theme_clearBtn    : [ "icon", "class" ],
+	theme_closeBtn    : [ "icon", "class" ],
+	theme_cancelBtn   : [ "icon", "class" ],
+	theme_tomorrowBtn : [ "icon", "class" ],
+	theme_todayBtn    : [ "icon", "class" ],
+
+	theme_dropdownContainer : "classList",
+	theme_modalContainer    : "classList",
+	theme_inlineContainer   : "classList",
+
+	theme_headerTheme  : "class",
+	theme_headerBtn    : [ "icon", "class" ],
+	theme_openButton   : "class",
+
+	theme_cal_Today       : "class",
+	theme_cal_DayHigh     : "class",
+	theme_cal_Selected    : "class",
+	theme_cal_DateHigh    : "class",
+	theme_cal_DateHighAlt : "class",
+	theme_cal_DateHighRec : "class",
+	theme_cal_Default     : "class",
+	theme_cal_OutOfBounds : "class",
+
+	theme_cal_NextBtn : [ "icon", "class" ],
+	theme_cal_PrevBtn : [ "icon", "class" ],
+
+	theme_cal_Pickers  : "class",
+	theme_cal_DateList : "class",
+
+	theme_dbox_NextBtn : [ "icon", "class" ],
+	theme_dbox_PrevBtn : [ "icon", "class" ],
+	theme_dbox_Inputs     : "class",
+
+	theme_fbox_Selected   : "class",
+	theme_fbox_Default    : "class",
+	theme_fbox_Forbidden  : "class",
+	theme_fbox_RollHeight : "135px",
+
+	theme_slide_Today       : "class",
+	theme_slide_DayHigh     : "class",
+	theme_slide_Selected    : "class",
+	theme_slide_DateHigh    : "class",
+	theme_slide_DateHighAlt : "class",
+	theme_slide_DateHighRec : "class",
+	theme_slide_Default     : "class",
+
+	theme_slide_NextBtn     : [ "icon", "class" ],
+	theme_slide_PrevBtn     : [ "icon", "class" ],
+	theme_slide_NextDateBtn : [ "icon", "class" ],
+	theme_slide_PrevDateBtn : [ "icon", "class" ],
+
+	theme_slide_Pickers  : false, // UNUSED
+	theme_slide_DateList : false, // UNUSED
+
+	theme_backgroundMask : {
+		position          : "fixed",
+		left              : 0,
+		top               : 0,
+		right             : 0,
+		bottom            : 0,
+		backgroundColor   : "rgba(0,0,0,.4)"
+	},
+	theme_headStyle : false,
+	theme_spanStyle : false,
+
+	buttonIconDate : "calendar",
+	buttonIconTime : "clock",
+
+	disabledState  : "disabled",
+
+	clickEvent : "click",
+	tranDone   : "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend"
+});
+
+/* eslint-disable no-unused-vars */
+
+/*
+ * Find the attacment point for the control
+ * 
+ * @param {boolean} isInline control is being inlined
+ * @returns {object} jQuery attachment point
+ */
+JTSageDateBox.style_attach = function( isInline ) {
+	
+};
+
+/*
+ * Make a button
+ * 
+ * @param  {array} theme Theme class and icon for the button
+ * @param  {string} contents Text contents of the button (if any)
+ * @return {string} Created button
+ */
+
+JTSageDateBox.style_btn = function( theme, contents ) {
+	
+};
+
+/*
+ * Make a button group
+ * 
+ * @param  {boolean} collapse Attempt to display buttons on one line
+ * @return {object} jQuery object of a button group that buttons can be appended to
+ */
+JTSageDateBox.style_btnGrp = function ( collapse ) {
+
+};
+
+/*
+ * Make a button group, outside wrapper (optional!!)
+ * 
+ * @param {boolean} collapse Collapse to one line
+ * @param {object} inner Inner HTML (jQuery)
+ * @return {object} jQuery
+ */
+JTSageDateBox.style_btnGrpOut = function ( collapse, inner ) {
+	
+},
+
+/*
+ * Wrap the original input in a div so we can add a button to it
+ * 
+ * @param  {object} originalInput Original input element, jQuery object
+ * @return {object} jQuery object now wrapped with some sort of div
+ */
+JTSageDateBox.style_inWrap = function ( originalInput ) {
+	
+};
+
+/*
+ * Create the open button that is added to the input
+ * 
+ * MUST contain dbOpenButton class. (outer)
+ * 
+ * @param  {string} icon Icon to use (name or SVG)
+ * @param  {string} title Hover text for the button
+ * @return {string} Rendered HTML of the open button
+ */
+JTSageDateBox.style_inBtn = function ( icon, title, theme ) {
+	
+};
+
+/*
+ * When not using the open button, we may need to alter the wrap class differently
+ * 
+ * @param  {object} originalInputWrap jQuery object
+ */
+JTSageDateBox.style_inNoBtn = function ( originalInputWrap ) {
+	
+};
+
+/*
+ * Hide the input element completely.
+ */
+JTSageDateBox.style_inHide = function() {
+	
+};
+
+/*
+ * Make the header for every mode
+ * 
+ * Close button MUST include the "dbCloser" class.
+ * 
+ * @param  {string} text Text of the header
+ * @param  {string} themeBar Theme class for the header
+ * @param  {string} themeButton Icon & Theme for the close button
+ * @return {string} Rendered HTML
+ */
+JTSageDateBox.style_mainHead = function ( text, themeBar, themeButton ) {
+	
+},
+
+/*
+ * Make an internal header ( datebox & flipbox )
+ * 
+ * MUST have the "dbHeader" class
+ * 
+ * @param  {string} text Text to display
+ * @return {object} jQuery object
+ */
+JTSageDateBox.style_subHead =  function ( text ) {
+	
+},
+
+/*
+ * Make the header for calbox / slidebox
+ * 
+ * @param  {string} txt Text to display
+ * @param  {string} prevBtn Previous button icon (name or SVG) & class
+ * @param  {string} nextBtn Next button icon (name or SVG) & class
+ * @param  {string} prevCtl Control class for previous button
+ * @param  {string} nextCtl Control class for next button
+ * @return {object} jQuery Object
+ */
+JTSageDateBox.style_pnHead = function ( txt, prevBtn, nextBtn, prevCtl, nextCtl ) {
+	
+};
+
+/*
+ * Create the year and month picker for calbox / slidebox
+ * 
+ * @param {object} ranges Year and Month arrays
+ * @param {array} ranges.year Containing arrays of [ value, text, selected (boolean) ]
+ * @param {array} ranges.month Containing arrays of [ value, text, selected (boolean) ]
+ * @param {string} theme Theme class for controls
+ * @param {string} monthCtl Control class for month picker
+ * @param {string} yearCtl Control class for year picker
+ * @return {object} jQuery Object
+ */
+JTSageDateBox.style_picker =  function ( ranges, theme, monthCtl, yearCtl ) {
+	
+};
+
+/*
+ * Make the calbox/slidebox drop down quick pick list.
+ * 
+ * Consider using {@link JTSageDateBox.html#._stdSel__anchor|_stdSel()}
+ *
+ * @param {string} listLabel Label for the list
+ * @param {array} list Containing arrays of [ value, text, selected (boolean) ]
+ * @param {string} theme Theme class
+ * @param {string} ctlCls Control class for select
+ * @return {object} jQuery Object
+ */
+JTSageDateBox.style_dateList = function ( listLabel, list, theme, ctlCls ) {
+	
+};
+
+
+/* CalBox Specific */
+
+
+/*
+ * Create the calbox grid container.  Probably a table
+ * 
+ * @return {object} jQuery object
+ */
+JTSageDateBox.style_calGrid = function () {
+	
+};
+
+/*
+ * Create a calbox grid row.  Probably a tr
+ * 
+ * @return {object} jQuery object
+ */
+JTSageDateBox.style_calRow = function () {
+	
+};
+
+/*
+ * Create a clickable box for each grid item in calbox.
+ * 
+ * MUST have the "dbEvent" class
+ * 
+ * @param {object}  data             Date information object
+ * @param {boolean} data.bad         True if the date is invalid
+ * @param {boolean} data.good        True if the date is valid
+ * @param {string}  data.theme       Theme class for the button
+ * @param {string}  data.displayText Text of the date
+ * @param {object}  data.dateObj     Date object
+ * 
+ * @param  {number} totalElements Number of elements in the row ( 7 or 8 )
+ * @return {object} jQuery Object
+ */
+JTSageDateBox.style_calBtn =  function ( data, totalElements ) {
+	
+};
+
+/*
+ * Create a non-button calbox grid box
+ * 
+ * @param  {string} text Text to display
+ * @param  {boolean} header Is this a header (bold?)
+ * @param  {number} totalElements Number of elements in the row ( 7 or 8 )
+ * @return {object} jQuery object
+ * @memberof JTSageDateBox.styleFunctions
+ * @this JTSageDateBox.styleFunctions
+ */
+JTSageDateBox.style_calTxt = function ( text, header, totalElements ) {
+	
+};
+
+
+/* DateBox Specific */
+
+/*
+ * Make the datebox mode container.
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_dboxCtr = function () {
+	
+};
+
+/*
+ * Make the datebox control row
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_dboxRow = function () {
+	
+};
+
+/*
+ * Make a datebox +/-/input control
+ * 
+ * Next button MUST have "dbBoxNext" class
+ * Previous button MUST have "dbBoxPrev" class
+ * Container must have "mainCls"
+ *
+ * @param {string} prevBtn Previous button icon (name or SVG) & class
+ * @param {string} nextBtn Next button icon (name of SVG) & class
+ * @param {string} mainCls Class for the control (input type)
+ * @param {string} label Label, if needed
+ * @param {string} inTheme Theme for inputs
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_dboxCtrl = function ( prevBtn, nextBtn, mainCls, label, /* inTheme */ ) {
+	
+};
+
+
+/* SlideBox Specific */
+
+/*
+ * Make the grid container for slidebox (usually a table)
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_slideGrid = function () {
+	
+};
+
+/*
+ * Make the grid for for slidebox (usually a TR)
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_slideRow = function () {
+	
+};
+
+/*
+ * Create a clickable box for each grid date in slidebox.
+ * 
+ * MUST have the "dbEventS" class
+ * 
+ * @param {object} data Date information object
+ * @param {boolean} data.bad True if the date is invalid
+ * @param {boolean} data.good True if the date is valid
+ * @param {string} data.theme Theme class for the button
+ * @param {object} data.dateObj Date object
+ * 
+ * @return {object} jQuery Object
+ */
+JTSageDateBox.style_slideBtn = function ( data ) {
+	
+};
+
+/*
+ * Create next/prev week buttons for slidebox
+ *
+ * @param {string} eventCls The event class.  Should be placed on the button
+ * @param {string} theme Icon to use for button ( name or SVG ) & theme
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_slideCtrl = function ( eventCls, theme ) {
+	
+};
+
+
+/* FlipBox Specific */
+
+
+
+/*
+ * Make the container for the flipbox
+ *
+ * @param {number} size Height CSS property
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxCtr = function ( size ) {
+	
+};
+
+/*
+ * Make a container for flipbox labels
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxDurLbls = function ( ) {
+	
+};
+
+/*
+ * Make a flibox label
+ *
+ * @param {string} text Text of the label
+ * @param {number} items Total number of items
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxDurLbl = function ( text, items ) {
+	
+};
+
+/*
+ * Make a flipbox roller container (outermost)
+ *
+ * @param {number} total Number of items
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxRollCtr = function ( /* total */ ) {
+	
+};
+
+/*
+ * Make a flipbox roller container (middle) - usually a UL
+ *
+ * @returns {object} jQuery Object
+ */	
+JTSageDateBox.style_fboxRollPrt = function () {
+	
+};
+
+/*
+ * Make a flipbox element (innermost) - usually a LI
+ *
+ * @param {string} text
+ * @param {string} cls
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxRollCld = function ( text, cls ) {
+	
+};
+
+/*
+ * Make the flipbox lens
+ *
+ * @returns {object} jQuery Object
+ */
+JTSageDateBox.style_fboxLens = function () {
+	
+};
+
+
+/*
+ * Position the flip elements.  Overrides the base function if it exists
+ */
+JTSageDateBox.style_fboxPos = function () {
+	
+};
+
