d2421521040ca2beb6fc847fe269470e1589a8a7
J.T.Sage <jtsage+datebox@gmail.com>
2019-05-30 13:29:57 -0400

    Add object.assign polyfill to jQM build. Re: #473

--- src/js/framework/jqm.js
+++ src/js/framework/jqm.js
@@ -713,3 +713,42 @@ JTSageDateBox.style_fboxPos = function () {
 };
 
 
+/*
+* Object.assign polyfill.  Provided only for jQM, support for other frameworks 
+* should be provided via HTML5 is ES5 shims if you are targeting older devices.
+*/
+
+/* eslint-disable one-var, no-unused-vars */
+
+if ( typeof Object.assign != "function" ) {
+	// Must be writable: true, enumerable: false, configurable: true
+	Object.defineProperty( Object, "assign", {
+		value : function assign( target, varArgs ) { // .length of function is 2
+			"use strict";
+			if ( target == null ) { // TypeError if undefined or null
+				throw new TypeError( "Cannot convert undefined or null to object" );
+			}
+		
+			var to = Object(target);
+		
+			for ( var index = 1; index < arguments.length; index++ ) {
+				var nextSource = arguments[index];
+  
+				if ( nextSource != null ) { // Skip over if undefined or null
+					for ( var nextKey in nextSource ) {
+						// Avoid bugs when hasOwnProperty is shadowed
+						if ( Object.prototype.hasOwnProperty.call( nextSource, nextKey ) ) {
+							to[nextKey] = nextSource[nextKey];
+						}
+					}
+				}
+			}
+		
+			return to;
+		},
+		writable : true,
+		configurable : true
+	});
+}
+
+/* eslint-enable one-var, no-unused-vars */
