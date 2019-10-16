9b00bd1d9d7821a3a4832f03fd7e3ea98620e184
J.T.Sage <jtsage+datebox@gmail.com>
2019-10-16 19:43:31 -0400

    Bump jsDoc on most, 5.2.0

--- src/js/lib/shortUtil.js
+++ src/js/lib/shortUtil.js
@@ -8,7 +8,7 @@
  * @author J.T.Sage <jtsage+datebox@gmail.com>
  * @author {@link https://github.com/jtsage/jtsage-datebox/contributors|GitHub Contributors}
  * @license {@link https://github.com/jtsage/jtsage-datebox/blob/master/LICENSE.txt|MIT}
- * @version 5.0.0
+ * @version 5.2.0
  */
 
 /* JTSage-DateBox 
--- src/js/lib/widgetDestroyEnableDisable.js
+++ src/js/lib/widgetDestroyEnableDisable.js
@@ -4,7 +4,7 @@
  * @author J.T.Sage <jtsage+datebox@gmail.com>
  * @author {@link https://github.com/jtsage/jtsage-datebox/contributors|GitHub Contributors}
  * @license {@link https://github.com/jtsage/jtsage-datebox/blob/master/LICENSE.txt|MIT}
- * @version 5.0.0
+ * @version 5.2.0
  */
 
 /**
